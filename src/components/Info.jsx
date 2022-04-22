5948e2d7c2e00dfd74a12177071c9757862107d7
Roman Diachenko <dyaroman@gmail.com>
2022-04-22 18:21:07 +0300

    add env to info component
    app also will be use with prod env

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,10 +2,11 @@ import { useSelector } from 'react-redux';
 
 export function Info() {
   const { preparedData, websitesData } = useSelector((state) => state['table']);
-  const { timestamp = '', commit = '', repoPath = '' } = websitesData;
+  const { timestamp = '', commit = '', repoPath = '', ENV = '' } = websitesData;
 
   return (
     <section className="info">
+      {ENV && <h4>ENV: {ENV}</h4>}
       {timestamp && <h4>Data last updated: {timestamp}</h4>}
       {repoPath && commit && (
         <h4>
