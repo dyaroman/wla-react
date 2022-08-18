e58526bfef4cd74776531b399db9fa0a1dd1aea5
Roman Diachenko <dyaroman@gmail.com>
2022-08-18 10:01:10 +0300

    refactoring ColorCell

--- src/components/ColorCell.jsx
+++ src/components/ColorCell.jsx
@@ -1,18 +1,38 @@
+import { useState } from 'react';
+
 import { fromCamelCaseToWords } from '../misc/functions';
 import { getContrastColor } from '../misc/color';
+import { NO_DATA } from '../misc/constants';
 
 export function ColorCell({ column, color, children }) {
-  const styles = {};
-  if (color.startsWith('#')) {
+  const [transparent, setTransparent] = useState(false);
+  const styles = {
+    transition: '0.3s',
+  };
+
+  let title = null;
+  if (color !== NO_DATA) {
+    title = `click to copy: ${color}`;
     styles.color = getContrastColor(color);
     styles.backgroundColor = color;
   }
 
+  if (transparent) {
+    styles.color = '#000';
+    styles.backgroundColor = transparent;
+  }
+
   async function onClick() {
+    if (color === NO_DATA) return;
     try {
+      setTransparent(true);
       await navigator.clipboard.writeText(color);
     } catch (e) {
       console.log(`Error due to copy websites list to clipboard`, e);
+    } finally {
+      setTimeout(() => {
+        setTransparent(false);
+      }, 300);
     }
   }
 
@@ -21,7 +41,7 @@ export function ColorCell({ column, color, children }) {
       data-title={fromCamelCaseToWords(column)}
       data-qa={column}
       style={styles}
-      title={`click to copy: ${color}`}
+      title={title}
       onClick={onClick}
     >
       {children}
