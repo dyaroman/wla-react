9c5448cbc11ed6ab00f4f7b340b7a22d4aaf82ec
Roman Diachenko <dyaroman@gmail.com>
2024-04-07 11:52:42 +0300

    add gtm event for shortcut open info modal

--- src/components/InfoModalBtn.jsx
+++ src/components/InfoModalBtn.jsx
@@ -3,12 +3,18 @@ import { useDispatch } from 'react-redux';
 import { useKeyPress } from '../hooks/useKeyPress';
 import { TOGGLE_INFO_MODAL } from '../features/app/app.constants';
 import { triggerGtmEvent } from '../misc/functions';
-import { OPEN_INFO_MODAL } from '../misc/gtm.constants';
+import { OPEN_INFO_MODAL, SHORTCUT } from '../misc/gtm.constants';
 
 export function InfoModalBtn() {
   const dispatch = useDispatch();
 
-  useKeyPress('shift+?', onOpenModalClick);
+  useKeyPress('shift+?', (event) => {
+    event.preventDefault();
+    onOpenModalClick();
+    triggerGtmEvent(SHORTCUT, {
+      method: 'open-info-modal',
+    });
+  });
 
   function onOpenModalClick() {
     dispatch({
