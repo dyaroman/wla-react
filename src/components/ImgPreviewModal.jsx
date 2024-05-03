522ba0b95056ca41581625d7533937cbedfb9023
Roman Diachenko <dyaroman@gmail.com>
2024-05-03 09:14:26 +0300

    rework imgPreviewModal

--- src/components/ImgPreviewModal.jsx
+++ src/components/ImgPreviewModal.jsx
@@ -6,32 +6,36 @@ import { TOGGLE_IMG_PREVIEW_MODAL } from '../features/app/app.constants';
 export function ImgPreviewModal() {
   const dispatch = useDispatch();
   const imgUrl = useSelector((state) => state['app'].imgPreviewUrl);
-  const dialog = useRef(null);
+  const dialogRef = useRef(null);
+  const cssClass = 'img-preview-modal';
 
   useEffect(() => {
     document.addEventListener('click', globalClickHandler);
-    dialog.current?.addEventListener('keydown', keyDownHandler);
     return () => {
       document.removeEventListener('click', globalClickHandler);
-      dialog.current?.removeEventListener('keydown', keyDownHandler);
     };
   }, []);
 
   useEffect(() => {
     if (imgUrl) {
-      dialog.current?.showModal();
+      dialogRef.current?.addEventListener('keydown', keyDownHandler);
+      dialogRef.current?.showModal();
     } else {
-      dialog.current?.close();
+      dialogRef.current?.close();
+      dialogRef.current?.removeEventListener('keydown', keyDownHandler);
     }
   }, [imgUrl]);
 
   function globalClickHandler(event) {
-    if (event.target !== dialog.current) return;
+    if (event.target !== dialogRef.current) return;
     onCloseModalClick();
   }
 
   function keyDownHandler(event) {
+    if (event.target.closest(`dialog.${cssClass}`) !== dialogRef.current)
+      return;
     if (event.key !== 'Escape') return;
+    if (!imgUrl) return;
     onCloseModalClick();
   }
 
@@ -45,7 +49,7 @@ export function ImgPreviewModal() {
   if (!imgUrl) return null;
 
   return (
-    <dialog className="img-preview-modal" ref={dialog}>
+    <dialog className={cssClass} ref={dialogRef}>
       <section className="img-preview-modal__content">
         <img src={imgUrl} alt="" className="img-preview-modal__image" />
 
