283ef4f72ce4ebb5775d9d455c74dc339a72010b
Roman Diachenko <dyaroman@gmail.com>
2024-08-20 15:44:32 +0300

    update 'thunk' import after update package

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 import { rootReducer } from './reducer';
 
