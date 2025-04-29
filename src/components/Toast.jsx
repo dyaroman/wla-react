5f38748fcc714e6c5338ac80b4b3af8eeab05bb9
Roman Diachenko <dyaroman@gmail.com>
2025-04-29 17:23:49 +0300

    rework Toast

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,13 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 
 import { hideToast, setCurrentToast } from '../features/toast/toast.actions';
 
-// todo: rework Toast to use createPortal to body and don't render when it don't need
 export function Toast() {
   const dispatch = useDispatch();
   const queue = useSelector((state) => state.toast.queue);
   const current = useSelector((state) => state.toast.current);
+  const [visible, setVisible] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
 
   useEffect(() => {
     let hideTimer, showTimer;
@@ -20,8 +27,12 @@ export function Toast() {
     }
 
     if (current) {
+      setVisible(true);
       hideTimer = setTimeout(() => {
-        dispatch(hideToast());
+        setVisible(false);
+        setTimeout(() => {
+          dispatch(hideToast());
+        }, 300); // Wait for animation to complete before hiding
       }, 5000);
     }
 
@@ -31,7 +42,16 @@ export function Toast() {
     };
   }, [queue, current, dispatch]);
 
-  return (
-    <div className={'toast' + (current ? ' show' : ' hide')}>{current}</div>
-  );
+  // Don't render anything if there's no toast and no queue
+  if (!current && queue.length === 0) {
+    return null;
+  }
+
+  // Use createPortal to render to body
+  return mounted
+    ? createPortal(
+        <div className={`toast ${visible ? 'show' : 'hide'}`}>{current}</div>,
+        document.body,
+      )
+    : null;
 }
