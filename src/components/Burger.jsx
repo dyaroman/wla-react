099c700379e5a61794c27c391875915e886abf60
Roman Diachenko <dyaroman@gmail.com>
2025-05-19 17:54:27 +0300

    rework burger
    * remove open animation
    * add hover state

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,21 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { openDrawer } from '../features/drawer/drawer.actions';
 import { SIDEBAR } from '../features/drawer/drawer.constants';
 
 export function Burger() {
   const dispatch = useDispatch();
-  const openDrawerId = useSelector((state) => state['drawer'].openDrawerId);
 
   function onClick() {
     dispatch(openDrawer(SIDEBAR));
   }
 
   return (
-    <button
-      className={'burger' + (openDrawerId === SIDEBAR ? ' burger--open' : '')}
-      onClick={onClick}
-      data-qa="burger"
-    >
+    <button className="burger" onClick={onClick} data-qa="burger">
       <span />
       <span />
       <span />
