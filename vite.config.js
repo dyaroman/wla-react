b97a3518b7269c07781deec023c9ad63e11d1403
Roman Diachenko <dyaroman@gmail.com>
2026-05-23 18:11:15 +0300

    setup github action

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { execSync } from 'child_process';
 
+const base = '/wla-react/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base,
   build: {
     outDir: 'build',
     sourcemap: true,
@@ -14,7 +17,7 @@ export default defineConfig({
       execSync('git rev-parse HEAD').toString().trim().slice(0, 8),
     ),
     __PAGE_TITLE__: JSON.stringify('WLA'),
-    __WEBSITES_DATA_URL__: JSON.stringify('/data'),
+    __WEBSITES_DATA_URL__: JSON.stringify(`${base}data`),
     __WLA_BACKEND_URL__: JSON.stringify(
       'https://gkrmkgvtltnbkwkqdxfw.supabase.co/functions/v1/wla-api',
     ),
